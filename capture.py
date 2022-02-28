5c0dc69ae11f2baaeb9fddd2399d8557f8640842
7eu7d7 <228218809@qq.com>
2022-02-28 09:05:55 +0800

    update capture

--- capture.py
+++ capture.py
@@ -1,8 +1,9 @@
 import time
-from utils import cap
+from utils import *
 
 import keyboard
 import winsound
+import cv2
 
 '''i=0
 while True:
@@ -11,8 +12,14 @@ while True:
     img.save(f'img_tmp/{i}.png')
     i+=1'''
 
+im_exit = cv2.imread('./imgs/exit.png')
+
 print('ok')
 keyboard.wait('t')
+
+exit_pos = match_img(cap_raw(), im_exit)
+gvars.genshin_window_rect_img = (exit_pos[0] - 32, exit_pos[1] - 19, DEFAULT_MONITOR_WIDTH, DEFAULT_MONITOR_HEIGHT)
+
 for i in range(56,56+20):
     img = cap()
     img.save(f'fish_dataset/{i}.png')
